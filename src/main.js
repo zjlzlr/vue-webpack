713d33dfa0c6d50f8392d9b19d155fee5fa80a3d
zhulirong <zhulirong@renrenche.com>
2016-11-13 00:00:46 +0800

    路由修改

--- src/main.js
+++ src/main.js
@@ -12,63 +12,75 @@ import Main from 'src/main.vue'
 // 每条路由规则应该映射到一个组件。这里的“组件”可以是一个使用 Vue.extend创建的组件构造函数，也可以是一个组件选项对象。
 // 稍后我们会讲解嵌套路由
 const router = new VueRouter({
-	routes: [
+    mode: 'history',
+    routes: [
         {
-	      path: '/index',//访问地址
-	      name: 'index',//定义路由的名字。方便使用。
-	      component: Index,//引用的组件名称，对应上面使用`import`导入的组件
-          //component:require("components/app.vue")//还可以直接使用这样的方式也是没问题的。不过会没有import集中引入那么直观
-	      
-	    },
-	    {
-	      path: '/one',//访问地址
-	      name: 'one',//定义路由的名字。方便使用。
-	      component: require('./views/one/index.vue'),//引用的组件名称，对应上面使用`import`导入的组件
-	      children: [
-		      {
-		        name: 'itemone',
-		        path: 'itemone',
-		        component: require('./views/one/itemone.vue')
-		      },
-		       {
-		        name: 'itemtwo',
-		        path: 'itemtwo',
-		        component: require('./views/one/itemtwo.vue')
-		      },
-		      {
-		        name: 'itemthree',
-		        path: 'itemthree',
-		        component: require('./views/one/itemthree.vue')
-		      },
-		    ]
-	    },
-	    {
-	      path: '/two',//访问地址
-	      name: 'two',//定义路由的名字。方便使用。
-	      component: require('./views/two/index'),//引用的组件名称，对应上面使用`import`导入的组件
-	    },
+            path: '/index',//访问地址
+            name: 'index',//定义路由的名字。方便使用。
+            component: Index,//引用的组件名称，对应上面使用`import`导入的组件
+            //component:require("components/app.vue")//还可以直接使用这样的方式也是没问题的。不过会没有import集中引入那么直观
 
-	    /*{
-	      path: '/list',//访问地址
-	      name: 'list',//定义路由的名字。方便使用。
-	      component: List,//引用的组件名称，对应上面使用`import`导入的组件
-	    },
-	    {
-	      path: '/hello',//访问地址
-	      name: 'hello',//定义路由的名字。方便使用。
-	      component: Hello,//引用的组件名称，对应上面使用`import`导入的组件
-	    },*/
-	    { path: '*', redirect: '/index' }
-	]
+        },
+        {
+            path: '/one',//访问地址
+            name: 'one',//定义路由的名字。方便使用。
+            component: function (resolve) {
+                require(['./views/one/index.vue'], resolve) // 引用的组件名称，对应上面使用`import`导入的组件
+            },
+            children: [
+                {
+                    name: 'itemone',
+                    path: '/itemone',
+                    component: function (resolve) {
+                        require(['./views/one/itemone.vue'], resolve)
+                    }
+                },
+                {
+                    name: 'itemtwo',
+                    path: '/itemtwo',
+                    component: function(resolve) {
+                        require(['./views/one/itemtwo.vue'], resolve)
+                    }
+                },
+                {
+                    name: 'itemthree',
+                    path: '/itemthree',
+                    component: function(resolve) {
+                        require(['./views/one/itemthree.vue'], resolve)
+                    }
+                },
+            ]
+        },
+        {
+            path: '/two',//访问地址
+            name: 'two',//定义路由的名字。方便使用。
+            component: function(resolve) {
+                require(['./views/two/index'], resolve) //引用的组件名称，对应上面使用`import`导入的组件
+            }
+        },
+
+        /*{
+         path: '/list',//访问地址
+         name: 'list',//定义路由的名字。方便使用。
+         component: List,//引用的组件名称，对应上面使用`import`导入的组件
+         },
+         {
+         path: '/hello',//访问地址
+         name: 'hello',//定义路由的名字。方便使用。
+         component: Hello,//引用的组件名称，对应上面使用`import`导入的组件
+         },*/
+        {path: '', redirect:'/index'}
+    ]
 });
 
 //开启debug模式
 Vue.config.debug = true;
 /*router.redirect({//定义全局的重定向规则。全局的重定向会在匹配当前路径之前执行。
-    '*':"/app"//重定向任意未匹配路径到/index
-});*/
+ '*':"/app"//重定向任意未匹配路径到/index
+ });*/
 // 现在我们可以启动应用了！
 const App = new Vue({
-  router,
-  ...Main,
-}).$mount('#app')
+    router,
+    ...Main,
+}).
+$mount('#app')
