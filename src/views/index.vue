608e8c6c40eb5c0d83e9f0fc8ecff8c7d1434a9a
zhulirong <zhulirong@renrenche.com>
2016-11-22 10:42:27 +0800

    edit

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,22 @@
 <template>
 <div class='app-index'>
     vue main index content
+
 </div>
 </template>
 <script>
     export default {//这里是官方的写法，默认导出，ES6
-       
+        data(){
+            return{
+              test:0
+            }
+        },
+        methods: {
+            go(){
+            },
+            mounted() {
+                // this.getBusiCollumn();
+            },
+        }
     }
 </script>
