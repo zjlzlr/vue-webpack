b5615cc7a384b742621a1d5561b0c7c965b14be9
zhulirong <zhulirong@renrenche.com>
2016-11-25 12:15:34 +0800

    element ui范例

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,30 @@
 <template>
 <div class='app-index'>
-    vue main index content
+    <el-tabs type="border-card" class="tabs">
+        <el-tab-pane label="用户管理">
+            <div>aaaaaaaaaaaaaaaaaaaaaaaaa</div>
+        </el-tab-pane>
+        <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+    </el-tabs>
 
+    <el-row>
+        <el-col :span="24"><div class=" demo-color-box bg-blue-light">sssssssssssssss</div></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="4"><div>===aa===dd===ee==</div></el-col>
+        <el-col :span="4"><div>aaaaaaaa</div></el-col>
+        <el-col :span="4"><div>bbbb</div></el-col>
+        <el-col :span="4"><div>ccccccccc</div></el-col>
+        <el-col :span="4"><div>ddddddddd</div></el-col>
+        <el-col :span="4"><div>eeeeeeee</div></el-col>
+    </el-row>
+    vue main index content
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="search">搜索</el-button>
 </div>
 </template>
 <script>
@@ -20,3 +43,8 @@
         }
     }
 </script>
+<style scoped lang="less">
+    .tabs{
+        widows: 100%;
+    }
+</style>
